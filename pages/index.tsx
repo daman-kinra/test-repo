f5cdb6a50973436fd34c96b1f4dd402d67ec9231
Daman kinra <daman.kinra@gmail.com>
2022-05-14 20:28:50 +0530

    added image component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,114 +1,29 @@
 import React from "react";
+import Image from "next/image";
+function index(props: any) {
+  console.log(props);
+  const arr = new Array(100).fill(0);
 
-function index() {
   return (
     <div>
-      <img
-        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
-        alt=""
-      />
-      <img
-        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
-        alt=""
-      />{" "}
-      <img
-        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
-        alt=""
-      />{" "}
-      <img
-        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
-        alt=""
-      />{" "}
-      <img
-        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
-        alt=""
-      />{" "}
-      <img
-        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
-        alt=""
-      />{" "}
-      <img
-        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
-        alt=""
-      />{" "}
-      <img
-        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
-        alt=""
-      />{" "}
-      <img
-        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
-        alt=""
-      />{" "}
-      <img
-        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
-        alt=""
-      />{" "}
-      <img
-        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
-        alt=""
-      />{" "}
-      <img
-        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
-        alt=""
-      />{" "}
-      <img
-        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
-        alt=""
-      />{" "}
-      <img
-        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
-        alt=""
-      />{" "}
-      <img
-        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
-        alt=""
-      />{" "}
-      <img
-        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
-        alt=""
-      />{" "}
-      <img
-        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
-        alt=""
-      />{" "}
-      <img
-        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
-        alt=""
-      />{" "}
-      <img
-        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
-        alt=""
-      />{" "}
-      <img
-        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
-        alt=""
-      />{" "}
-      <img
-        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
-        alt=""
-      />{" "}
-      <img
-        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
-        alt=""
-      />{" "}
-      <img
-        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
-        alt=""
-      />{" "}
-      <img
-        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
-        alt=""
-      />{" "}
-      <img
-        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
-        alt=""
-      />{" "}
-      <img
-        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
-        alt=""
-      />
+      {arr.map((item, index) => {
+        return (
+          <Image
+            loading="lazy"
+            key={index}
+            src={
+              "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
+            }
+            width={500}
+            height={500}
+          />
+        );
+      })}
     </div>
   );
 }
 
+export const getStaticProps = (ctx: any) => {
+  return { props: { data: "abc" } };
+};
 export default index;
